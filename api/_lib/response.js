42226d7d6873c16f9094e26fb4149680a2494d02
Shilong Chen <aa619333213@gmail.com>
2026-04-04 19:55:35 +0800

    Fix missing frontend datetime helper

--- api/_lib/response.js
+++ api/_lib/response.js
@@ -22,6 +22,9 @@ function sendJson(res, statusCode, data) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
   res.send(JSON.stringify(data));
 }
 
--- shared.js
+++ shared.js
@@ -599,6 +599,24 @@
     riskBadgeClass,
     sentimentLabel,
     relativeDate,
+    formatDateTime(value) {
+      if (!value) {
+        return "暂无";
+      }
+
+      const date = new Date(value);
+      if (Number.isNaN(date.getTime())) {
+        return "暂无";
+      }
+
+      return new Intl.DateTimeFormat("zh-CN", {
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      }).format(date);
+    },
     getDefaultRules,
   };
 })();
